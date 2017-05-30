1fd80ce9419bf69fc1704b554349735b217fe382
Padraic <padraic.histon@gmail.com>
2017-05-30 12:56:54 +0100

    Added ToDo Model.

---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+
+        // The User property below will allow us to link a user to the item when it is created
+        // so that only items created by the user can be seen by that user.
+        public virtual ApplicationUser User { get; set; }
+    }
+}
